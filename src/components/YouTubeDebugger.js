e7aa3edcd5a942d17a317984aeba2762f120aadd
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-25 15:10:00 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,24 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super()
+
+    state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+
+    
+  }
+
+  
+
+}
